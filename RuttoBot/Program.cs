1050575d26ba306c0bbf67107aa86a7dd82669f4
NiiloVan <niilo.vanhatupa@entteri.fi>
2023-03-30 13:30:24 +0300

    Bot structure ready

--- RuttoBot/Program.cs
+++ RuttoBot/Program.cs
@@ -1,2 +1,76 @@
-﻿gith// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RuttoBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().StartnRunBotAsync().GetAwaiter().GetResult();
+
+        DiscordSocketClient client;
+        CommandService commands;
+        IServiceProvider services;
+
+        public async Task StartnRunBotAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+
+            string botToken = "MTA5MDkyODU0MTE1MzA5OTg3OQ.GJvXBG.vuXlOzONmioylqkxzL9hcQN_eRZMtgDCYOLTwo";
+
+            client.Log += Client_Log;
+
+            await RegisterCommansAsync();
+
+            await client.LoginAsync(TokenType.Bot, botToken);
+
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommansAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix("?", ref argPos))
+            {
+                var result = await commands.ExecuteAsync(context,argPos,services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+
+        }
+
+    }
+    
+    
+    
+}
