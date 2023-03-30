80c177b8490ea9a7c65dfa04bf811d7f4607296d
NiiloVan <niilo.vanhatupa@entteri.fi>
2023-03-30 14:25:40 +0300

    adadaddad

---
+++ RuttoBotNet/Dbot.cs
@@ -0,0 +1,33 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuttoBotNet
+{
+    public class Dbot
+    {
+        public DiscordClient DiscordClient { get; private set; }
+        public InteractivityExtension interactivityExtension { get; private set; }
+        public CommandsNextExtension Commands {get ; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+            
+            var configJson = JsonConvert.
+        }
+
+    }
+}
